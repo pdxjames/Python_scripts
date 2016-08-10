5d899fb0d277db21f885358711e64dce199bfd6a
pdxjames <james.dishongh@gmail.com>
2016-08-09 23:59:19 -0700

    Random number generator
    A simple (convoluted) random number generator that's really not too random.

---
+++ rng.py
@@ -0,0 +1,12 @@
+#random number generator
+print ""
+print "Welcome to my random number generator."
+print "-----------"*3
+print ""
+x = int(raw_input("Enter a number between 0 and 10:  "))
+y = int(raw_input("Now enter a number between 5 and 10:  "))
+name = raw_input("So by the way, what's your name? ")
+
+r = (x*20)/(y+17) + 4
+print "Well", name, "your random number is:"
+print r
